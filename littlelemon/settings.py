fb3e247897f9d6c63a5e8aa5a4943ec77b5ece25
#V0ID <hey@v0id.me>
2025-02-05 02:22:38 +0300

    test: add MenuTest to verify Menu model item representation

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -86,9 +86,13 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myapp',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
